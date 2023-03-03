0ca0c06f84e4d4735b13931d2d16859c1eeb565b
jing-jin-mc <45392705+jing-jin-mc@users.noreply.github.com>
2023-03-03 15:21:08 +0100

    Update streamlit_app.py
    get data into a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,23 @@
 import streamlit
 import snowflake.connector
-import pandas
+import pandas as pd
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 ### connect to snowflake ###
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+
+# run a snowflake query and put it in a car 
+my_cur.execute("SELECT color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+# put the data into a dataframe and visulize it 
+df = pd.DataFrame(my_catalog)
+streamlit.write(df)
+
+
+
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)

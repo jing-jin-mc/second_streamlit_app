d167864616a5a8d5e5bbf2dd91f6cb73bcb70d4b
jing-jin-mc <45392705+jing-jin-mc@users.noreply.github.com>
2023-03-03 15:30:16 +0100

    Update streamlit_app.py
    fix write function bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_catalog = my_cur.fetchall()
 
 # put the data into a dataframe and visulize it 
 df = pd.DataFrame(my_catalog)
-streamlit.write('We have ' + len(df) + ' colors to choose!')
+streamlit.write('Number of  colors to choose' , len(df) )
 
 # get a color list and put it into a select box
 color_list = df[0].values.tolist()

23652ec222a1b26a0c334aba67c6245d4ea3a388
jing-jin-mc <45392705+jing-jin-mc@users.noreply.github.com>
2023-03-03 15:15:54 +0100

    Update streamlit_app.py
    import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import snowflake.connector
+import pandas
+
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 ### connect to snowflake ###
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -9,4 +12,3 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-streamlit.title('My Parents New Healthy Diner')

dc577647f586e2a9d0128946d9cfe8015182a71e
jing-jin-mc <45392705+jing-jin-mc@users.noreply.github.com>
2023-03-03 15:25:30 +0100

    Update streamlit_app.py
    get images

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import snowflake.connector
 import pandas as pd
 
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
+streamlit.title('Amazing Athleisure Catalog')
 
 ### connect to snowflake ###
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -16,9 +16,16 @@ my_catalog = my_cur.fetchall()
 df = pd.DataFrame(my_catalog)
 streamlit.write(df)
 
+# get a color list and put it into a select box
+color_list = df[0].values.tolist()
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
+# build the image caption
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+streamlit.image( df2[0],width=400, caption= product_caption )
+streamlit.write('Price: ', df2[1]) streamlit.write('Sizes Available: ',df2[2]) streamlit.write(df2[3])
 
